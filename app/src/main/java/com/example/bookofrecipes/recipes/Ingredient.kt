885eff652681a64ef563bc1a0d5a093f79bb65f2
DeadDev <deaddev@duck.com>
2024-07-05 18:18:37 +0300

    Added foreign keys logic

--- app/src/main/java/com/example/bookofrecipes/recipes/Ingredient.kt
+++ app/src/main/java/com/example/bookofrecipes/recipes/Ingredient.kt
@@ -2,11 +2,21 @@ package com.example.bookofrecipes.recipes
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Fts4
 import androidx.room.PrimaryKey
 
 @Fts4
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Recipe::class,
+            parentColumns = ["rowid"],
+            childColumns = ["recipe_id"],
+            onDelete = ForeignKey.NO_ACTION
+        )
+    ]
+)
 data class Ingredient(
     @PrimaryKey @ColumnInfo(name = "rowid") val id: Long,
     @ColumnInfo(name = "recipe_id") val recipeId: Long,
--- app/src/main/java/com/example/bookofrecipes/recipes/RecipeStep.kt
+++ app/src/main/java/com/example/bookofrecipes/recipes/RecipeStep.kt
@@ -2,9 +2,19 @@ package com.example.bookofrecipes.recipes
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Recipe::class,
+            parentColumns = ["rowid"],
+            childColumns = ["recipe_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class RecipeStep(
     @PrimaryKey val id: Long,
     @ColumnInfo(name = "recipe_id") val recipeId: Long,
