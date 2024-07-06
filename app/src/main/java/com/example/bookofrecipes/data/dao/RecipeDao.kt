8601270c3217f4df375fcf5aa2728381dcc2a1fa
DeadDev <deaddev@duck.com>
2024-07-06 15:50:19 +0300

    Removed unused import directives

--- app/src/main/java/com/example/bookofrecipes/data/dao/RecipeDao.kt
+++ app/src/main/java/com/example/bookofrecipes/data/dao/RecipeDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import com.example.bookofrecipes.data.models.Ingredient
 import com.example.bookofrecipes.data.models.Recipe
 
 @Dao
--- app/src/main/java/com/example/bookofrecipes/data/models/Ingredient.kt
+++ app/src/main/java/com/example/bookofrecipes/data/models/Ingredient.kt
@@ -1,8 +1,6 @@
 package com.example.bookofrecipes.data.models
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.Fts4
 import androidx.room.PrimaryKey
 
 @Entity(tableName="ingredients")
