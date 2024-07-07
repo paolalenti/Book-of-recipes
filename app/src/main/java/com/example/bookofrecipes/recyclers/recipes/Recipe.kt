7e22385b3679a2f26da9d2a46c92183672896f7c
teruuj1 <shuvalova05sofya@mail.ru>
2024-07-07 22:14:26 +0300

    Recipe data class changed

--- app/src/main/java/com/example/bookofrecipes/recyclers/recipes/Recipe.kt
+++ app/src/main/java/com/example/bookofrecipes/recyclers/recipes/Recipe.kt
@@ -1,10 +1,12 @@
 package com.example.bookofrecipes.recyclers.recipes
 
+import com.example.bookofrecipes.recyclers.recipes.recyclers.steps.RecipeStep
+
 data class Recipe(
     val id: Int,
     val name: String,
     val time: String,
-    val ingredients: List<String>,
+    val ingredients: List<RecipeStep>,
     val steps: List<String>,
     val url: String
 )
