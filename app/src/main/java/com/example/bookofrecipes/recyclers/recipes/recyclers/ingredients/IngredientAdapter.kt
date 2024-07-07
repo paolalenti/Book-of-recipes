7b832dfe6d9f1f5dba547a644e5b0d1435eabf4e
teruuj1 <shuvalova05sofya@mail.ru>
2024-07-07 21:59:23 +0300

    Recipe`s ingredients Holder and Adapter added

---
+++ app/src/main/java/com/example/bookofrecipes/recyclers/recipes/recyclers/ingredients/IngredientAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.bookofrecipes.recyclers.recipes.recyclers.ingredients
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookofrecipes.databinding.ItemIngredientBinding
+
+class IngredientAdapter(
+    private val ingredients: List<String>
+) : RecyclerView.Adapter<IngredientHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): IngredientHolder = IngredientHolder(
+        binding = ItemIngredientBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+    )
+
+    override fun onBindViewHolder(holder: IngredientHolder, position: Int) {
+        holder.onBind(ingredients[position])
+    }
+
+    override fun getItemCount(): Int = ingredients.size
+}
