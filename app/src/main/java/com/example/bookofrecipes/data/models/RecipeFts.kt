28425f4722996c26ac5c69962d856cf474c96784
DeadDev <deaddev@duck.com>
2024-07-09 14:10:40 +0300

    FTS tables docs

--- app/src/main/java/com/example/bookofrecipes/data/models/RecipeFts.kt
+++ app/src/main/java/com/example/bookofrecipes/data/models/RecipeFts.kt
@@ -3,6 +3,12 @@ package com.example.bookofrecipes.data.models
 import androidx.room.Entity
 import androidx.room.Fts4
 
+/**
+ * Full text search table for recipe
+ *
+ * @property name Fts4 should index name of recipe
+ * @property description Fts4 should index description
+ */
 @Fts4(contentEntity = Recipe::class)
 @Entity(tableName = "recipe_fts")
 data class RecipeFts(
