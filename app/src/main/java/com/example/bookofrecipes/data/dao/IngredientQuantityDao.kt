fb3dc5831704b2912f9c0ec0fad985a403d3d1be
DeadDev <deaddev@duck.com>
2024-07-09 15:01:22 +0300

    IngredientQuantityDao KDoc

--- app/src/main/java/com/example/bookofrecipes/data/dao/IngredientQuantityDao.kt
+++ app/src/main/java/com/example/bookofrecipes/data/dao/IngredientQuantityDao.kt
@@ -7,20 +7,52 @@ import androidx.room.Query
 import androidx.room.Update
 import com.example.bookofrecipes.data.models.IngredientQuantity
 
+/**
+ * Ingredient quantity data access object
+ *
+ */
 @Dao
 interface IngredientQuantityDao {
+    /**
+     * Insert new quantity into database
+     *
+     * @param ingredientQuantity Quantities to insert
+     * @return Array of quantity indices
+     */
     @Insert
     fun insert(vararg ingredientQuantity: IngredientQuantity): Array<Long>
 
+    /**
+     * Get all quantities for recipe
+     *
+     * @param recipeId Recipe identifier
+     * @return List of quantities
+     */
     @Query("SELECT * FROM ingredients_quantities WHERE recipe_id = :recipeId")
     fun getAllByRecipeId(recipeId: Long): List<IngredientQuantity>
 
+    /**
+     * Get all quantities for ingredient
+     *
+     * @param ingredientId Ingredient identifier
+     * @return List of quantities
+     */
     @Query("SELECT * FROM ingredients_quantities WHERE ingredient_id = :ingredientId")
     fun getAllByIngredientId(ingredientId: Long): List<IngredientQuantity>
 
+    /**
+     * Update given quantity
+     *
+     * @param ingredientQuantity Quantity to update
+     */
     @Update
     fun update(ingredientQuantity: IngredientQuantity)
 
+    /**
+     * Delete given quantity
+     *
+     * @param ingredientQuantity Quantity to delete
+     */
     @Delete
     fun delete(ingredientQuantity: IngredientQuantity)
 }
