01624d7659608903c2c0c5524c62257a6d2e952b
DeadDev <deaddev@duck.com>
2024-07-09 14:31:13 +0300

    Ingredient KDoc

--- app/src/main/java/com/example/bookofrecipes/data/models/Ingredient.kt
+++ app/src/main/java/com/example/bookofrecipes/data/models/Ingredient.kt
@@ -4,6 +4,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.Index
 
+/**
+ * Ingredients database model
+ * This need to be a separate model to perform searching and filtering by ingredient
+ *
+ * @property id Unique index of ingredient
+ * @property name Name of ingredient
+ */
 @Entity(tableName = "ingredients", indices = [Index(value = ["name"], unique = true)])
 data class Ingredient(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
