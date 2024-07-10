d89a949912f0a140d6334bc2cb8514779ce9b0b0
paolalenti <kirill251104@gmail.com>
2024-07-10 18:32:32 +0300

    fixed onBackPressed from HomeAdvancedFragment to RecipeEditorFragment

--- app/src/main/java/com/example/bookofrecipes/fragments/HomeAdvancedFragment.kt
+++ app/src/main/java/com/example/bookofrecipes/fragments/HomeAdvancedFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
